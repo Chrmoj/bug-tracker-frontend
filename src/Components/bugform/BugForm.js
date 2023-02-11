c31328d5df43e2da965a34eb7470248a98ef1383
unknown <christian.f.mojica@gmail.com>
2023-02-10 19:49:53 -0500

    Changed register page & signIn page

--- src/Components/bugform/BugForm.js
+++ src/Components/bugform/BugForm.js
@@ -1,15 +1,14 @@
-import './BugForm.css';
-import { useState } from 'react';
-import api from '../../api/axiosConfig';
+import "./BugForm.css";
+import { useState } from "react";
+import api from "../../api/axiosConfig";
 
-export const BugForm = ({toggleForm}) => {
-  const [bug, setBug] = useState ({
-    priority:"Low",
-    description:"",
-    status:"Solved",
-    dueDate:"",
+export const BugForm = ({ toggleForm }) => {
+  const [bug, setBug] = useState({
+    priority: "Low",
+    description: "",
+    status: "Solved",
+    dueDate: "",
   });
-
 
   const { priority, description, status, dueDate } = bug;
 
@@ -29,59 +28,67 @@ export const BugForm = ({toggleForm}) => {
     refreshPage();
   };
   return (
-    <form className="bug-form" onSubmit={(event)=>onSubmit(event)}>
+    <form className="bug-form" onSubmit={(event) => onSubmit(event)}>
       <h2>Submit a new Bug</h2>
       <br></br>
       <div>
-
-       <label for="priority">Priority:</label>
-    <select id="priority" name="priority" required="true"
-      value={priority}
-      onChange={(event)=>onInputChange(event)}>
-    <option value="low">low</option>
-    <option value="medium">medium</option>
-    <option value="high">high</option>
-<br></br>
-  </select>
-        <textarea 
-        placeholder='Description'
-        className='description-field'
-        name='description'
-        required='true'
-        value={description}
-        onChange={(event)=>onInputChange(event)}>
-        </textarea>
+        <label for="priority">Priority:</label>
+        <select
+          id="priority"
+          name="priority"
+          required="true"
+          value={priority}
+          onChange={(event) => onInputChange(event)}
+        >
+          <option value="low">low</option>
+          <option value="medium">medium</option>
+          <option value="high">high</option>
+          <br></br>
+        </select>
+        <br />
+        <textarea
+          placeholder="Description"
+          className="description-field"
+          name="description"
+          required="true"
+          value={description}
+          onChange={(event) => onInputChange(event)}
+        ></textarea>{" "}
       </div>
       <div>
         <label for="status">Status:</label>
-    <select id="status" name="status" required ="true"
-        value={status}
-        onChange={(event)=>onInputChange(event)}>
-
-      <option value="Solved">Solved</option>
-      <option value="Blocked">Blocked</option>
-      <option value="In Progress">In Progress</option>
-      <option value="Needs Review">Needs Review</option>
-      <option value="Unclaimed">Unclaimed</option>
-      <option value="Up Next">Up Next</option>
-
-    </select>
+        <select
+          id="status"
+          name="status"
+          required="true"
+          value={status}
+          onChange={(event) => onInputChange(event)}
+        >
+          <option value="Solved">Solved</option>
+          <option value="Blocked">Blocked</option>
+          <option value="In Progress">In Progress</option>
+          <option value="Needs Review">Needs Review</option>
+          <option value="Unclaimed">Unclaimed</option>
+          <option value="Up Next">Up Next</option>
+        </select>
       </div>
       <div>
-        <input type="Date"
-        placeholder='Due Date'
-        className='form-field'
-        name ='dueDate'
-        required='true'
-        value={dueDate}
-        onChange={(event)=>onInputChange(event)}></input>
+        <input
+          type="Date"
+          placeholder="Due Date"
+          className="form-field"
+          name="dueDate"
+          required="true"
+          value={dueDate}
+          onChange={(event) => onInputChange(event)}
+        ></input>
       </div>
       <input
         type="Submit"
         className="submit-btn"
         defaultValue="Submit Bug"
-      ></input> 
-      </form>
-  )
-}
+      ></input>
+    </form>
+  );
+};
 export default BugForm;
--- src/Components/signin/SignIn.js
+++ src/Components/signin/SignIn.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import api from "../../api/axiosConfig";
+
+import "./SignIn.css";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-export default SignIn
+  const { email, password } = user;
+
+  const onInputChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await api.post("/api/v1/auth/register", user);
+    <Link to="/signin"></Link>;
+  };
+
+  return (
+    <div className=" container-signIn">
+      <div className="row">
+        <div className="col">
+          <h2>Sign In</h2>
+          <form onSubmit={(event) => onSubmit(event)}>
+            <div>
+              <label className="form-label">E-mail</label>
+              <br />
+              <input
+                type={"email"}
+                className="form-control"
+                placeholder="Enter your e-mail address"
+                name="email"
+                value={email}
+                onChange={(event) => onInputChange(event)}
+              ></input>
+            </div>
+            <div>
+              <div>
+                <label for="password">Password (8 characters minimum):</label>
+                <br />
+                <input
+                  type="password"
+                  id="pass"
+                  name="password"
+                  minLength="8"
+                  required
+                  value={password}
+                  onChange={(event) => onInputChange(event)}
+                ></input>
+              </div>
+            </div>
+            <div className="buttons-register">
+              <Link className="sign-in" to="/signin">
+                Sign In
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
